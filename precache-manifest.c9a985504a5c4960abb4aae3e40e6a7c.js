7f3d4c0615dc58adb22bf722ab1d50708901d9dd
Nabil-Carel <nabilgbaguidi8@gmail.com>
2019-08-23 15:25:53 -0400

    Updates

---
+++ precache-manifest.c9a985504a5c4960abb4aae3e40e6a7c.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7248614171aadb2eac42fb036fbaeb99",
+    "url": "/voting-app/index.html"
+  },
+  {
+    "revision": "c2f3e9cc30d1039458c0",
+    "url": "/voting-app/static/css/2.3cef9883.chunk.css"
+  },
+  {
+    "revision": "ec92558ec27e354434dd",
+    "url": "/voting-app/static/css/main.eba83494.chunk.css"
+  },
+  {
+    "revision": "c2f3e9cc30d1039458c0",
+    "url": "/voting-app/static/js/2.09d1baa8.chunk.js"
+  },
+  {
+    "revision": "ec92558ec27e354434dd",
+    "url": "/voting-app/static/js/main.01d9e530.chunk.js"
+  },
+  {
+    "revision": "6b9d1523c98de6352a4b",
+    "url": "/voting-app/static/js/runtime~main.852d6adc.js"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/voting-app/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/voting-app/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/voting-app/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/voting-app/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/voting-app/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "fcfe2bd1975fa2db1ff60aa0ccdd2349",
+    "url": "/voting-app/static/media/daniel.fcfe2bd1.jpg"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/voting-app/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/voting-app/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/voting-app/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/voting-app/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/voting-app/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/voting-app/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "3ac267b2778ccaaf0425ef1b0c4aade6",
+    "url": "/voting-app/static/media/kristy.3ac267b2.png"
+  },
+  {
+    "revision": "45fdd64889fe43b886545dce5d85b93d",
+    "url": "/voting-app/static/media/molly.45fdd648.png"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/voting-app/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/voting-app/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/voting-app/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/voting-app/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/voting-app/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "c60fcb0be09e491814a8f4253973a83b",
+    "url": "/voting-app/static/media/veronika.c60fcb0b.jpg"
+  }
+]);
